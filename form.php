01290508b1e082181ae5b9980cc4ea09abc9fe23
matheus <matheusalcantaradelima1997@gmail.com>
2019-09-26 20:32:31 -0300

    adicionado required no form e manxlength

--- form.php
+++ form.php
@@ -18,22 +18,24 @@
         <!--FORM pessoa física-->
         <form action="salvaForm.php" method="post" id="formFisica">
             <div class="form-group">
-                <label><input type="text" name="nome" placeholder="Digite seu nome" class="form-control" size="100" maxlength="100"></label><br>
-                <label><input type="text" name="email" placeholder="Digite seu e-mail" class="form-control" size="100" maxlength="100"></label><br>
-                <label><input type="text" name="dataNasci" placeholder="Digite sua data de nascimento" class="form-control" size="100" maxlength="100"></label><br>
-                <label><input type="text" name="cpf" placeholder="Digite seu CPF" class="form-control" size="100" maxlength="100"></label><br>
-                <label><input type="password" name="senhaPF" placeholder="Digite sua senha" class="form-control" size="100" maxlength="100"></label><br>
+                <label><input type="text" name="nome" placeholder="Digite seu nome" class="form-control" size="100" maxlength="50" required></label><br>
+                <label><input type="text" name="email" placeholder="Digite seu e-mail" class="form-control" size="100" maxlength="100" required></label><br>
+                <label><input type="text" name="dataNasci" placeholder="Digite sua data de nascimento" class="form-control" size="100"  maxlength="8" required></label><br>
+                <label><input type="text" name="cpf" placeholder="Digite seu CPF" class="form-control" size="100" maxlength="11" required></label><br>
+                <label><input type="password" name="senhaPF" placeholder="Digite sua senha" class="form-control" size="100" maxlength="20" required></label><br>
             </div>
             <input type="submit" class="btn btn-primary">
         </form>
         <!--FORM pessoa jurídica-->
         <form action="salvaForm.php" method="post" id="formJuridica">
             <div class="form-group">
-                <label><input type="text" name="nomeEstacionamento" placeholder="Digite o Nome do estacionamento" class="form-control" size="100"></label><br>
-                <label><input type="text" name="email" placeholder="Digite o e-mail da empresa" class="form-control" size="100"></label><br>
-                <label><input type="text" name="cnpj" placeholder="Digite o CNPJ" class="form-control" size="100"></label><br>
-                <label><input type="text" name="endereco" placeholder="Digite o endereço" class="form-control" size="100"></label><br>
-                <label><input type="password" name="senhaPJ" placeholder="Digite a senha" class="form-control" size="100"></label><br>
+                <label><input type="text" name="nomeEstacionamento" placeholder="Digite o Nome do estacionamento" class="form-control" size="100" maxlength="40" required></label><br>
+                <label><input type="text" name="email" placeholder="Digite o e-mail da empresa" class="form-control" size="100"  maxlength="100" required></label><br>
+                <label><input type="text" name="cnpj" placeholder="Digite o CNPJ" class="form-control" size="100"  maxlength="19" required></label><br>
+                <label><input type="text" name="cep" placeholder="Digite o CEP" class="form-control" size="100"  maxlength="9" required></label><br>
+                <label><input type="text" name="complemento" placeholder="Digite o complemento" class="form-control" size="100"  maxlength="30"></label><br>
+                <label><input type="text" name="numero" placeholder="Digite o número" class="form-control" size="100"  maxlength="5" required></label><br>
+                <label><input type="password" name="senhaPJ" placeholder="Digite a senha" class="form-control" size="100"  maxlength="20" required></label><br>
             </div>
             <input type="submit" class="btn btn-primary">
         </form>
