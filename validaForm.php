be8f673cf83170fbafd986d45eeda296a797b8ab
matheus <matheusalcantaradelima1997@gmail.com>
2019-09-30 20:43:19 -0300

    resolvido o problema da validação da data

--- validaForm.php
+++ validaForm.php
@@ -9,17 +9,19 @@
     //Delimitando a TIMEZONE, linha importante para fazer comparativos de datas a partir do php 6
     date_default_timezone_set('America/Sao_Paulo');
     $data = $_POST['dataNasci'];
-    $sysData = date("Y/m/d");
-    $idade = date_diff(strtotime($data), strtotime($sysData));// date diff pega a diferença da tada em dias
+    $sysData = date("Y-m-d");
     //$teste = date("Y/m/d");
     //echo '<script>alert('.($data-$teste).')</script>';
-    if(strtotime($data) < strtotime($sysData) && $idade >= 6570){
+    if($data < $sysData){
         echo '<script>alert("DATA VÁLIDA")</script>';
-        echo '<script>alert("MAIOR DE IDADE")</script>';
-        header('Location: /logar.php');
-    }else if(strtotime($data) > strtotime($sysData) && $idade < 6570){
-      echo '<script>alert("MENOR DE IDADE")</script>';
+        //header('Location: /logar.php');
+        echo  var_dump($data);
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        echo var_dump($sysData);
+    }else{
       echo '<script>alert("DATA INVÁLIDA")</script>';
-      header('Location: /form.php');
+      //header('Location: /form.php');
     }
  ?>
