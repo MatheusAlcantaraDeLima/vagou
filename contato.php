e91522957958a0c34d34a7e0a0af80e69b38a9de
matheus <matheusalcantaradelima1997@gmail.com>
2019-10-04 20:35:12 -0300

    modificado a página contato

--- contato.php
+++ contato.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<!--Adicionado CSS-->
-	<link rel="stylesheet" href="css/corpo.css">
 	<!--Adicionado CDN do Bootstrap-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!--Adicionado CSS-->
+	<link rel="stylesheet" href="css/estiloImgGrupo.css">
 	<title>VaGou!</title>
 </head>
 <body>
@@ -13,62 +13,28 @@
 			include_once("menu.php");
 		?>
 		<hr>
-		<div style="text-align:center;">
-			<h3>Equipe do VaGou!</h3>
-		</div>
-		<div class="card-deck">
-			<div class="card border-primary" style="width: 12rem;">
-				<img src="img/equipe/matheus.png" class="card-img-top" alt="Imagem do matheus, membro da equipe de desenvolvimento do VaGou!">
-				<div class="card-body">
-					<strong>Matheus</strong>
-					<p>Equipe de desenvolvimento do VaGou!</p>
-					<div class="row">
-						<div class="col">
-							<a href="https://github.com/MatheusAlcantaraDeLima" target="_blank"><input type="button" value="GitHub" class="btn btn-primary"></a>
-						</div>
-						<div class="col">
-							<a href="https://www.linkedin.com/in/matheus-alcantara-119164164/" target="_blank"><input type="button" value="LinkedIn" class="btn btn-primary"></a>
-						</div>
-					</div>
+		<h3 style="text-align:center;">Equipe do VaGou!</h3>
+		<div class="row">
+				<div class="col">
+					<img src="img/equipe/matheus.png" alt="desenvolvedor" class="img_grupo">
+					<a href="https://github.com/MatheusAlcantaraDeLima" target="_blank" class="botao"><input type="button" value="GitHub" class="btn btn-primary"></a>
+					<a href="https://www.linkedin.com/in/matheus-alcantara-119164164/" target="_blank" class="botao"><input type="button" value="LinkedIn" class="btn btn-primary"></a>
 				</div>
-			</div>
-
-			<div class="card border-primary" style="width: 12rem;">
-				<img src="img/equipe/matheus.png" class="card-img-top" alt="Imagem do matheus, membro da equipe de desenvolvimento do VaGou!">
-				<div class="card-body">
-				<strong>Matheus</strong>
-					<p>Equipe de desenvolvimento do VaGou!</p>
-					<div class="row">
-						<div class="col">
-							<a href="https://github.com/MatheusAlcantaraDeLima" target="_blank"><input type="button" value="GitHub" class="btn btn-primary"></a>
-						</div>
-						<div class="col">
-							<a href="https://www.linkedin.com/in/matheus-alcantara-119164164/" target="_blank"><input type="button" value="LinkedIn" class="btn btn-primary"></a>
-						</div>
-					</div>
+				<div class="col">		
+					<img src="img/equipe/matheus.png" alt="desenvolvedor" class="img_grupo">
+					<a href="https://github.com/MatheusAlcantaraDeLima" target="_blank" class="botao"><input type="button" value="GitHub" class="btn btn-primary"></a>
+					<a href="https://www.linkedin.com/in/matheus-alcantara-119164164/" target="_blank" class="botao"><input type="button" value="LinkedIn" class="btn btn-primary"></a>
 				</div>
-			</div>
-
-			<div class="card border-primary" style="width: 12rem;">
-				<img src="img/equipe/matheus.png" class="card-img-top" alt="Imagem do matheus, membro da equipe de desenvolvimento do VaGou!">
-				<div class="card-body">
-				<strong>Matheus</strong>
-					<p>Equipe de desenvolvimento do VaGou!</p>
-					<div class="row">
-						<div class="col">
-							<a href="https://github.com/MatheusAlcantaraDeLima" target="_blank"><input type="button" value="GitHub" class="btn btn-primary"></a>
-						</div>
-						<div class="col">
-							<a href="https://www.linkedin.com/in/matheus-alcantara-119164164/" target="_blank"><input type="button" value="LinkedIn" class="btn btn-primary"></a>
-						</div>
-					</div>
+				<div class="col">
+					<img src="img/equipe/matheus.png" alt="desenvolvedor" class="img_grupo">
+					<a href="https://github.com/MatheusAlcantaraDeLima" target="_blank" class="botao"><input type="button" value="GitHub" class="btn btn-primary"></a>
+					<a href="https://www.linkedin.com/in/matheus-alcantara-119164164/" target="_blank" class="botao"><input type="button" value="LinkedIn" class="btn btn-primary"></a>
 				</div>
-			</div>
 		</div>
 	</div>
 	<!--Adicionando footer-->
     <?php
-        include_once("footer.php")
+        include_once("footer.php");
     ?>
 </body>
 </html>
--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!--Adicionado CSS-->
-<link rel="stylesheet" href="css/estiloFooter.css">
+<link rel="stylesheet" href="css/estiloRodape.css">
 <footer>
   <div class="col-6">
     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum.</p>
