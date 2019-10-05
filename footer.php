59adbc395d2b1c07f1f3e5c90b8b3da984929283
matheus <matheusalcantaradelima1997@gmail.com>
2019-10-05 13:59:57 -0300

    melhoraado o footer, e modificado o contato

--- footer.php
+++ footer.php
@@ -1,10 +1,20 @@
 <!--Adicionado CSS-->
 <link rel="stylesheet" href="css/estiloRodape.css">
 <footer>
-  <div class="col-6">
-    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum.</p>
-  </div>
-    <div class="col-6">
+  <div class="row">
+    <div class="col"></div>
+    <div class="col-4">
+      <h4>Um pouco sobre o VaGou!</h4>
+      <p>O VaGou! foi idealizado para ajudar vocês, nossos clientes a acharem um estacionamento com vaga o mais próximo e com o melhor preço. Este projeto foi construído no laboratório do Senac Niterói.</p>
+    </div>
+    <div class="col"></div>
+      <div class="col-4">
+      <h4>Links</h4>
+      <a href="index.php"><span style="color: white;">Home</span></a><br>
+      <a href="logar.php"><span style="color: white;">Logar-se</span></a><br>
+      <a href="form.php"><span style="color: white;">Registrar-se</span></a><br>
+      <a href="contato.php"><span style="color: white;">Contato</span></a>
+    </div>
   </div>
   </footer>
 
