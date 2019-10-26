a36c6174343b5d6d41d428efdbac261ed39f691c
matheus <matheusalcantaradelima1997@gmail.com>
2019-10-26 12:38:03 -0300

    resolvido o problema do login

--- validaLogin.php
+++ validaLogin.php
@@ -4,13 +4,16 @@
 
     include_once("conexao.php");
 
-    $querySelect = "select email, senha_client from cliente";
+    $querySelect = "select email, senha_client from cliente where email like '".$email."' ";
 
     $execucao = mysqli_query($conexao, $querySelect);
 
     $dados = mysqli_fetch_array($execucao);
+    
+    $emailDb = $dados['email'];
+    $senhaDb = $dados['senha_client'];
 
-    if($email == $dados['email'] && $senha == $dados['senha_client']){
+    if($email == $emailDb && $senha == $senhaDb){
         session_start();
         $_SESSION['email'] = $dados['email'];
         echo "<script>alert('Logado com Sucesso!')</script>";
@@ -18,6 +21,8 @@
     }else{
         echo "<script>alert('Erro ao logar-se.')</script>";
         echo "<script> window.location.href = 'logar.php'</script>";
+       echo $emailDb;
+       echo $senhaDb;
     }
     
 ?>
