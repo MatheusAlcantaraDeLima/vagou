d9de28a59fb4944c16e16ae3e6fc29a2f1939b63
matheus <matheusalcantaradelima1997@gmail.com>
2019-10-31 14:22:54 -0300

    modificado o footer

--- footer.php
+++ footer.php
@@ -11,12 +11,10 @@
 </style>
 <footer>
   <div class="row">
-    <div class="col"></div>
-    <div class="col-4">
+    <div class="col">
       <h4>Um pouco sobre o VaGou!</h4>
       <p>O VaGou! foi idealizado para ajudar vocês, nossos clientes a acharem um estacionamento com vaga o mais próximo e com o melhor preço. Este projeto foi construído no laboratório do Senac Niterói.</p>
     </div>
-    <div class="col"></div>
       <div class="col-4">
       <h4>Links</h4>
       <a href="index.php"><span style="color: white;">Home</span></a><br>
--- menu.php
+++ menu.php
@@ -9,20 +9,25 @@
 		<small><strong>Dê valor ao tempo que realmente importa!</strong></small>
 	</div>
 </div>
-<div class="col">
-	<ul class="nav">
-		<li class="nav-item">
-			<a class="btn btn-outline-primary nav-link" href="index.php">Home</a>
-		</li>
-		<li class="nav-item">
-			<a class="btn btn-outline-primary nav-link" href="logar.php">logar</a>
-		</li>
-		<li class="nav-item">
-			<a class="btn btn-outline-primary nav-link" href="form.php">Cadastrar</a>
-		</li>
-		<li class="nav-item">
-			<a class="btn btn-outline-primary nav-link" href="contato.php">Contato</a>
-		</li>
-	</ul>
-</div>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light ollapse navbar-collapse">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+  	</button>
+	<div class="col" id="navbarNavAltMarkup">
+		<ul class="nav">
+			<li class="nav-item">
+				<a class="nav-item nav-link active btn btn-outline-primary" href="index.php">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-item nav-link btn btn-outline-primary" href="logar.php">Logar</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-item nav-link btn btn-outline-primary" href="form.php">Cadastrar</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-item nav-link btn btn-outline-primary" href="contato.php">Contato</a>
+			</li>
+		</ul>
+	</div>
+</nav>
